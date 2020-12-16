e5d9fb142da86d27b35cbdda9a688f373d92e32c
Duncanson <duncanson@dxc.com>
2020-12-16 19:08:30 +0000

    Added agent search to create agent signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -24,10 +24,6 @@ def customer_profile(sender, instance, created, **kwargs):
 
         agentsearch.brands.set(Brand.objects.all())
 
-    agent = Agent.objects.get(user=instance)
-
-    agentsearch = AgentSearch.objects.get(agent=agent)
-
-    agentsearch.teams.set(agent.team_id)
+        agentsearch.brands.set(Team.objects.all())
 
 post_save.connect(customer_profile, sender=User)
