b291c1a359a5411df32f34673876f2f7656d65c2
Duncanson <duncanson@dxc.com>
2020-12-18 11:24:37 +0000

    Added env variable for SECRET_KEY

--- ServiceCallReport/settings.py
+++ ServiceCallReport/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5kuc7c+)9_p%*%!b&wr^)#%hbl1q$j5z3xx0k0lf_4c)!q+k#g'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
