75528e6534210324aa4e02aeb766065d9b9bdf95
Duncanson <duncanson@dxc.com>
2020-12-21 13:57:22 +0000

    Adding django-table feature

--- accounts/tables.py
+++ accounts/tables.py
@@ -7,7 +7,13 @@ class AgentContacts(tables.Table):
         fields = ["contact_id"]
 
 class AgentList(tables.Table):
+
+    my_extra_column = tables.Column(accessor='my_function',
+         verbose_name='My calculated value')
+
+    btnCol = tables.URLColumn("View")
+
     class Meta:
         model = Agent
-        fields = ["user", "user.first_name", "user.last_name", "user.email", "team_id", "team_id.department_id"]
+        fields = ["user", "user.first_name", "user.last_name", "user.email", "team_id", "team_id.department_id", {"btnCol": "{% url 'agent_page' id %}"}]
         attrs = {"class": "table table-sm"}
