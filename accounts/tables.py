28de94c0857bbf16cb5d159739b0e1fbb2f9c835
Duncanson <duncanson@dxc.com>
2020-12-21 22:44:55 +0000

    Adding django-table feature

--- accounts/tables.py
+++ accounts/tables.py
@@ -17,6 +17,18 @@ class AgentContacts(tables.Table):
 
     class Meta:
         model = ReqService
-        fields = ["contact_id", "contact_id.contact_date"]
+        fields = [
+            "contact_id",
+            "contact_id.contact_date",
+            "contact_id.call_time",
+            "contact_id.contact_session_id.call_start_time",
+            "contact_id.contact_session_id.wrap_up_duration",
+            "contact_id.contact_session_id.call_end_time",
+            "contact_id.call_outcome",
+            "contact_id.wrap_up_notes",
+            "contact_id.contact_session_id.call_type",
+            "contact_id.contact_session_id.dialled",
+            "contact_id.contact_session_id.brand_id",
+            ]
         attrs = {"class": "table table-sm"}
         orderable = False
