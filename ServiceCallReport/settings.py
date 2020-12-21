a983df13c0131be873af2c147729bdfa413516c1
Duncanson <duncanson@dxc.com>
2020-12-21 13:37:25 +0000

    Adding django-table feature

--- ServiceCallReport/settings.py
+++ ServiceCallReport/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'django_filters',
+    #'django_filters',
+    'django_tables2',
 ]
 
 MIDDLEWARE = [
