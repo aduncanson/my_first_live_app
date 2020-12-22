ff5927b982657f482bf06c95adeeb12186422e74
Duncanson <duncanson@dxc.com>
2020-12-22 11:17:38 +0000

    Added model functions to external file

--- accounts/tables.py
+++ accounts/tables.py
@@ -16,9 +16,9 @@ class AgentList(tables.Table):
 class AgentContactsTable(tables.Table):
 
     contact_date = tables.DateTimeColumn(format = 'N j, Y')
-    contact_session_id.call_start_time = tables.TimeColumn(format = 'g:i:s a')
-    contact_session_id.wrap_up_duration = tables.TimeColumn(format = 'G:i:s')
-    contact_session_id.call_end_time = tables.TimeColumn(format = 'g:i:s a')
+    contact_session_id__call_start_time = tables.TimeColumn(format = 'g:i:s a')
+    contact_session_id__wrap_up_duration = tables.TimeColumn(format = 'G:i:s')
+    contact_session_id__call_end_time = tables.TimeColumn(format = 'g:i:s a')
 
     class Meta:
         model = ClientContact
