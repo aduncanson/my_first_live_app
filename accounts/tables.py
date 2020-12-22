1ba1a7d49e1abe6e8410ca8205dee77efe9414fa
Duncanson <duncanson@dxc.com>
2020-12-22 16:02:09 +0000

    Added model functions to external file

--- accounts/tables.py
+++ accounts/tables.py
@@ -26,9 +26,6 @@ class AgentContactsTable(tables.Table):
             "contact_id",
             "contact_date",
             "call_time",
-            "contact_session_id__call_start_time",
-            "contact_session_id__wrap_up_duration",
-            "contact_session_id__call_end_time",
             "call_outcome",
             "wrap_up_notes",
             ]
