70b9a9f092ad778f17ae48c550af48203a2f84f3
Duncanson <duncanson@dxc.com>
2020-12-23 08:00:17 +0000

    Added model functions to external file

--- accounts/classes/reports.py
+++ accounts/classes/reports.py
@@ -29,6 +29,17 @@ def criteria_agent_contact(agent, report):
         call_time=F('contact_id_id__contact_session_id_id__call_end_time') - F('contact_id_id__contact_session_id_id__call_start_time')
     )
 
+    report_annotated = report_annotated.value(
+            "contact_id",
+            "contact_id__contact_date",
+            "call_time",
+            "contact_id__contact_session_id__call_start_time",
+            "contact_id__contact_session_id__wrap_up_duration",
+            "contact_id__contact_session_id__call_end_time",
+            "contact_id__call_outcome",
+            "contact_id__wrap_up_notes",
+    )
+
     report_annotated_filter = report_annotated.filter(
         call_time__range=[agent_search.call_lower_limit, agent_search.call_upper_limit],
         contact_id_id__contact_session_id__brand_id__in=agent_search.brands.all()
