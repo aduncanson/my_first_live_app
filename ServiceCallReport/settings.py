e26162795b405b6e670de92b0e60b837db67c6ff
Duncanson <duncanson@dxc.com>
2020-12-24 16:51:08 +0000

    Table format

--- ServiceCallReport/settings.py
+++ ServiceCallReport/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    #'django_filters',
-    'django_tables2',
+    'datatableview',
 ]
 
 MIDDLEWARE = [
