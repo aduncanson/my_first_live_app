4d145c156004a63d122f73ba48df480187f2acb7
Duncanson <duncanson@dxc.com>
2020-12-28 22:02:14 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -1,11 +1,20 @@
 from ajax_datatable.views import AjaxDatatableView
+from django.contrib.auth.models import Permission
 
 from .models import *
 
 class AgentListAjaxDatatableView(AjaxDatatableView):
 
     model = Agent
-    title = 'Agent'
-    #initial_order = [["user", "asc"], ]
+    title = 'Permissions'
+    initial_order = [["app_label", "asc"], ]
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
     search_values_separator = '+'
+
+    column_defs = [
+        {'name': 'id', 'visible': False, },
+        {'name': 'codename', 'visible': True, },
+        {'name': 'name', 'visible': True, },
+        {'name': 'app_label', 'foreign_field': 'content_type__app_label', 'visible': True, },
+        {'name': 'model', 'foreign_field': 'content_type__model', 'visible': True, },
+    ]
