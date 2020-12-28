2ef00e7a52bf83ad634dfd1688d6de28d16f53f1
Duncanson <duncanson@dxc.com>
2020-12-28 22:05:07 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -5,7 +5,7 @@ from .models import *
 
 class AgentListAjaxDatatableView(AjaxDatatableView):
 
-    model = Agent
+    model = Permission
     title = 'Permissions'
     initial_order = [["app_label", "asc"], ]
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
