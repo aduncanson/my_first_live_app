fe7eb89dd5ae1afff6260ac7535069766edf5120
Duncanson <duncanson@dxc.com>
2020-12-28 22:19:30 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -7,13 +7,10 @@ class AgentListAjaxDatatableView(AjaxDatatableView):
 
     model = Permission
     title = 'Permissions'
-    initial_order = [["app_label", "asc"], ]
+    initial_order = [["codename", "asc"], ]
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
     search_values_separator = '+'
 
     column_defs = [
         {'name': 'codename', 'visible': True, },
-        {'name': 'name', 'visible': True, },
-        {'name': 'app_label', 'foreign_field': 'content_type__app_label', 'visible': True, },
-        {'name': 'model', 'foreign_field': 'content_type__model', 'visible': True, },
     ]
