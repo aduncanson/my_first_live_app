1a3ed448f268ee4e0152976c84cfa52f2953ff7e
Duncanson <duncanson@dxc.com>
2020-12-28 23:36:21 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -7,7 +7,7 @@ class AgentListAjaxDatatableView(AjaxDatatableView):
 
     model = Agent
     title = 'Agent'
-    initial_order = [["user_id", "asc"], ]
+    initial_order = [["Username", "asc"], ]
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
     search_values_separator = '+'
 
@@ -16,4 +16,6 @@ class AgentListAjaxDatatableView(AjaxDatatableView):
         {'name': 'First Name', 'foreign_field': 'user__first_name', 'visible': True, },
         {'name': 'Surname', 'foreign_field': 'user__last_name', 'visible': True, },
         {'name': 'Email', 'foreign_field': 'user__email', 'visible': True, },
+        {'name': 'Team', 'foreign_field': 'team__team_name', 'visible': True, },
+        {'name': 'Department', 'foreign_field': 'team__department__department_name', 'visible': True, },
     ]
