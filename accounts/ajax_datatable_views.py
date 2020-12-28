9c4873949516589e96c9f8da2e9e520f7c37e2b2
Duncanson <duncanson@dxc.com>
2020-12-28 23:39:09 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -13,9 +13,4 @@ class AgentListAjaxDatatableView(AjaxDatatableView):
 
     column_defs = [
         {'name': 'Username', 'foreign_field': 'user__username', 'visible': True, },
-        {'name': 'First Name', 'foreign_field': 'user__first_name', 'visible': True, },
-        {'name': 'Surname', 'foreign_field': 'user__last_name', 'visible': True, },
-        {'name': 'Email', 'foreign_field': 'user__email', 'visible': True, },
-        {'name': 'Team', 'foreign_field': 'team__team_name', 'visible': True, },
-        {'name': 'Department', 'foreign_field': 'team__department__department_name', 'visible': True, },
     ]
