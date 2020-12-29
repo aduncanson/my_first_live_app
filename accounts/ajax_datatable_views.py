03e8cc6035ac7f62f358043ec47d70beb9d2c57a
Duncanson <duncanson@dxc.com>
2020-12-29 09:33:05 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -21,5 +21,5 @@ class AgentListAjaxDatatableView(AjaxDatatableView):
     ]
 
     def customize_row(self, row, obj):
-        row['Name'] = obj.user__email
+        row['Name'] = obj.id
         row['Agent Dashboard'] = '<a href="">View</a>'
