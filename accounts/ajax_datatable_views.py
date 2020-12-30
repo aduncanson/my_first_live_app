79cc6531f1d670a132569108bcb7434439b0e970
Duncanson <duncanson@dxc.com>
2020-12-30 19:20:29 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -59,9 +59,9 @@ class AgentContactsAjaxDatatableView(AjaxDatatableView):
         {'name': 'Call End Time', 'foreign_field': 'contact_session_id__call_end_time', 'visible': True, },
         {'name': 'call_outcome', 'visible': True, },
         {'name': 'wrap_up_notes', 'visible': True, },
-        {'name': 'Brand', 'foreign_field': 'contact_session_id__brand_id', 'visible': True, },
         {'name': 'Call Type', 'foreign_field': 'contact_session_id__call_type', 'visible': True, },
         {'name': 'Dialled', 'foreign_field': 'contact_session_id__dialled', 'visible': True, },
+        {'name': 'Brand', 'foreign_field': 'contact_session_id__brand_id', 'visible': True, },
         {'name': 'Comments', 'visible': True, },
     ]
 
