62d01af4b70857d5f480e99d765d401435ec6e34
Duncanson <duncanson@dxc.com>
2020-12-30 01:09:24 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -61,7 +61,7 @@ class AgentContactsAjaxDatatableView(AjaxDatatableView):
     ]
 
     def customize_row(self, row, obj):
-        row['Call Time'] = obj.contact_session_id__call_start_time
+        row['Call Time'] = obj.call_time
 
     def get_initial_queryset(self, request=None):
 
