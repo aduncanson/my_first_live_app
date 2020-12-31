5f13f02dde94d03fe21baca95a0b360e9f185dc7
Duncanson <duncanson@dxc.com>
2020-12-31 00:54:38 +0000

    Converting to ajax-datatables

--- accounts/ajax_datatable_views.py
+++ accounts/ajax_datatable_views.py
@@ -73,7 +73,7 @@ class AgentContactsAjaxDatatableView(AjaxDatatableView):
         )
 
         row['Call Time'] = str(obj.call_time)
-        row['Comments'] = service_model.dict()
+        row['Comments'] = service_model.values("comments")
 
     def get_initial_queryset(self, request=None):
 
