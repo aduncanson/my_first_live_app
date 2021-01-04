4b6c333e6b325f278cb5e4233091f142bb93b745
Duncanson <duncanson@dxc.com>
2021-01-04 16:43:54 +0000

    Converting to ajax-datatables

--- accounts/classes/reports.py
+++ accounts/classes/reports.py
@@ -44,8 +44,6 @@ def contact_reports(request, agent, start_date, end_date):
     call_outcome_table = all_reqservices.values(
         "contact_id",
         "contact_id__call_outcome"
-    ).annotate(
-        count=Count("req_service_id")
     )
 
     content = {
