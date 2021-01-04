528484ae7d99e1857c88698cd3aa59e7e17ed094
Duncanson <duncanson@dxc.com>
2021-01-04 22:13:19 +0000

    Converting to ajax-datatables

--- accounts/classes/reports.py
+++ accounts/classes/reports.py
@@ -41,11 +41,13 @@ def contact_reports(request, agent, start_date, end_date):
         contact_id__contact_session_id__brand_id__in=agent_search.brands.all()
     )
 
-    call_outcome_table = all_reqservices.values(
+    call_time_qs = all_reqservices.values(
         "contact_id__call_outcome",
     ).annotate(
         call_time=F('contact_id__contact_session_id__call_end_time') - F('contact_id__contact_session_id__call_start_time'),
-    ).values(
+    )
+
+    call_outcome_table = call_time_qs.values(
         "contact_id__call_outcome",
     ).annotate(
         full_count=Count("contact_id", distinct=True),
@@ -57,13 +59,31 @@ def contact_reports(request, agent, start_date, end_date):
                     then=F("contact_id")
                     ),
             output_field=IntegerField(),
-        ), distinct=True)
+        ),
+        distinct=True
+        )
+    )
+
+    services_table = call_time_qs.values(
+        "service_type_id__service_type_name",
+    ).annotate(
+        full_count=Count("req_service_id"),
+        criteria_count=Count(
+            Case(
+                When(
+                    call_time__range=[agent_search.call_lower_limit, agent_search.call_upper_limit],
+                    contact_id__contact_session_id__brand_id__in=agent_search.brands.all(),
+                    then=F("req_service_id")
+                    ),
+            output_field=IntegerField(),
+            )
+        )
     )
 
     content = {
         "criteria_contact_table": criteria_contact_table,
         "call_outcome_table": call_outcome_table,
-        #"services_table": services_table,
+        "services_table": services_table,
     }
 
     return content
