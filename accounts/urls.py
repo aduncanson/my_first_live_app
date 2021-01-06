678f91cee0be2992530dcfb36af359791db1787b
Duncanson <duncanson@dxc.com>
2021-01-06 21:21:25 +0000

    Experimenting with charts

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     # ajax
     path('ajax_datatable/agent_list/', ajax_datatable_views.AgentListAjaxDatatableView.as_view(), name="ajax_list_of_agents_table"),
 
-    # chartjs
-    path('pie-chart/', views.pie_chart, name='pie-chart'),
-
 ]
