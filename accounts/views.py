6366d4acee2fff6ef45f96fd1c3ef7df4a07ce7c
Duncanson <duncanson@dxc.com>
2021-01-10 02:44:59 +0000

    Experimenting with charts

--- accounts/views.py
+++ accounts/views.py
@@ -159,8 +159,8 @@ def dashboard(request):
     if request.method == "POST":
         date_form = FilterContactDate(request.POST)
         if date_form.is_valid():
-            start_date_time = FilterContactDate.cleaned_data['start_date_time']
-            end_date_time = FilterContactDate.cleaned_data['end_date_time']
+            start_date_time = date_form.cleaned_data['start_date_time']
+            end_date_time = date_form.cleaned_data['end_date_time']
 
     all_reports = contact_reports(request, None, start_date_time, end_date_time)
 
