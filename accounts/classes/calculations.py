166df3a139337f1bff7ec38e6d920382d4dcebae
Duncanson <duncanson@dxc.com>
2021-01-10 01:13:40 +0000

    Experimenting with charts

--- accounts/classes/calculations.py
+++ accounts/classes/calculations.py
@@ -14,7 +14,12 @@ def dailyStats(request, queryset):
         criteria_call_count=Count(
             Case(
                 When(
-                    call_time__range=[agent_search.call_lower_limit, agent_search.call_upper_limit],
+                    call_time__lte=agent_search.call_lower_limit,
+                    contact_id__contact_session_id__brand_id__in=agent_search.brands.all(),
+                    then=F("contact_id")
+                    ),
+                When(
+                    call_time__gte=agent_search.call_upper_limit,
                     contact_id__contact_session_id__brand_id__in=agent_search.brands.all(),
                     then=F("contact_id")
                     ),
