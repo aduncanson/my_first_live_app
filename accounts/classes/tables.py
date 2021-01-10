eafb5577ab56bb27dc7e34cc2cd1dd4ebd776a55
Duncanson <duncanson@dxc.com>
2021-01-10 01:20:55 +0000

    Experimenting with charts

--- accounts/classes/tables.py
+++ accounts/classes/tables.py
@@ -45,7 +45,8 @@ def contact_reports(request, agent, start_date, end_date):
     )
 
     criteria_contact_table = full_contact_table.filter(
-        call_time__range=[agent_search.call_lower_limit, agent_search.call_upper_limit],
+        call_time__lte=agent_search.call_lower_limit,
+        call_time__gte=agent_search.call_upper_limit,
         contact_id__contact_session_id__brand_id__in=agent_search.brands.all()
     )
 
