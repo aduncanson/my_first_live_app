9d5c7fcf38d65349d25b67809f936b58ee0b4445
Duncanson <duncanson@dxc.com>
2021-01-11 17:22:47 +0000

    Experimenting with charts

--- accounts/views.py
+++ accounts/views.py
@@ -267,7 +267,7 @@ def agentActivity(request, pk):
         services=ArrayAgg('service_type_id__service_type_name', ordering=("req_service_id")),
     )
 
-    username = contact_details["contact_id__agent__username"]
+    username = contact_details.get("contact_id__agent__username")
 
     context = {
         "title": title,
