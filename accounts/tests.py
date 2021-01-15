327dea653168816acecee384f3516d72f67f25d9
Duncanson <duncanson@dxc.com>
2021-01-15 10:50:00 +0000

    Add line break export functionality

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "..settings")
+import django
+django.setup()
+from django.test import TestCase, Client
+import unittest
 
-# Create your tests here.
+# my django imports
+from models import *
+
+class CreateUserFormTest(unittest.TestCase):
+
+    def test_new_agent_form(self):
+        return True
+
+if __name__ == "__main__":
+    unittest.main()
